ead51f1a01ec5ff072ce639abf825342faad924b
williamburger6 <williamburger6@ymail.com>
2020-09-24 20:14:57 -0400

    added robo super

--- src/main/java/project/RoboCat.java
+++ src/main/java/project/RoboCat.java
@@ -1,53 +1,10 @@
 package project;
 
-public class RoboCat {
-
-
-    private String name;
-    private String description;
-    private int oilLevel;
-    private int performanceLevel;
-    private int boredomLevel;
-
+public class RoboCat extends RoboPet {
 
 
     public RoboCat(String name, String description, int oilLevel, int performanceLevel, int boredomLevel) {
-        this.name = name;
-        this.description = description;
-        this.oilLevel = oilLevel;
-        this.performanceLevel = performanceLevel;
-        this.boredomLevel = boredomLevel;
+        super(name, description, oilLevel, performanceLevel, boredomLevel);
     }
 
-    public String getName(){
-        return name;
-    }
-
-    public String getDescription(){
-        return description;
-    }
-
-    public void oilPet() {
-        oilLevel -=3;
-    }
-
-    public int getOilLevel() {
-        return oilLevel;
-    }
-
-    public void maintenancePet() {
-        performanceLevel -=3;
-    }
-
-    public int getPerfomanceLevel() {
-        return performanceLevel;
-    }
-
-    public void playRoboPet() {
-        boredomLevel -= 3;
-    }
-
-    public int getBoredom() {
-        return boredomLevel;
-    }
 }
--- src/main/java/project/RoboDog.java
+++ src/main/java/project/RoboDog.java
@@ -1,55 +1,9 @@
 package project;
 
-public class RoboDog {
-
-    private String name;
-    private String description;
-    private int oilLevel;
-    private int performanceLevel;
-    private int boredomLevel;
-
-
-
+public class RoboDog extends RoboPet {
     public RoboDog(String name, String description, int oilLevel, int performanceLevel, int boredomLevel) {
-        this.name = name;
-        this.description = description;
-        this.oilLevel = oilLevel;
-        this.performanceLevel = performanceLevel;
-        this.boredomLevel = boredomLevel;
+        super(name, description, oilLevel, performanceLevel, boredomLevel);
     }
-
-    public String getName(){
-        return name;
-    }
-
-    public String getDescription(){
-        return description;
-    }
-
-    public void oilPet() {
-        oilLevel -=3;
-    }
-
-    public int getOilLevel() {
-        return oilLevel;
-    }
-
-    public void maintenancePet() {
-        performanceLevel -=3;
-    }
-
-    public int getPerfomanceLevel() {
-        return performanceLevel;
-    }
-
-    public void playRoboPet() {
-        boredomLevel -= 3;
-    }
-
-    public int getBoredom() {
-        return boredomLevel;
-    }
-
     public void walkPet() {
         boredomLevel =0;
         performanceLevel +=2;
